edec77626c973041385ad715024ff004b32cc02e
EliasYamil <eliasyamilaraya@gmail.com>
2022-10-27 05:36:12 -0300

    ads

---
+++ src/main/java/com/portfolio/elias/EliasApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.elias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliasApplication.class, args);
+	}
+
+}
